738190d1e83806d9834e4b3d50e13d3ffdf4072d
Hlebnov Alexandr <hlebnov-sasha@mail.ru>
2024-09-20 20:34:55 +0300

    Home work №1

--- google_should_find_selene.py
+++ google_should_find_selene.py
@@ -1,6 +1,6 @@
 from selene import browser, be, have
-pip install selene --pre
+
 
 browser.open('https://google.com')
 browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
